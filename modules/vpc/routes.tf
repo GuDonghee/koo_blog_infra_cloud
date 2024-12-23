75c2b80ca84f5a95c4c4a4e0ef6bb1c6e0b5da00
GuDonghee <dev.donghee1995@gmail.com>
2024-12-23 18:38:27 +0900

    feat: 라우팅테이블 생성

---
+++ modules/vpc/routes.tf
@@ -0,0 +1,35 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name      = "${var.vpc_name}-public-rt"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name      = "${var.vpc_name}-private-rt"
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets.cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets.cidrs)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
