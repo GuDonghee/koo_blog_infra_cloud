f89796117ebe3b6160f6d20d4372a5858ae8340a
GuDonghee <dev.donghee1995@gmail.com>
2025-02-16 13:32:05 +0900

    feat: EKS 노드그룹 생성

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -11,3 +11,25 @@ resource "aws_eks_cluster" "eks_cluster" {
     ManagedBy = "Terraform"
   }
 }
+
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  
+  node_group_name = "${var.eks_name}-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [ aws_eks_cluster.eks_cluster, aws_iam_role.eks_node_role ]
+
+  tags = {
+    Name      = "${var.eks_name}-node-group",
+    ManagedBy = "Terraform"
+  }
+}
